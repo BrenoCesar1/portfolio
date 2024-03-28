8d65c04b7571ea36b6346dce2b992a16acd651e1
breno.santos <breno.santos@meta.com.br>
2024-03-28 16:04:31 -0300

    Alterações nas Estilizações

--- src/componentes/PostCard/index.js
+++ src/componentes/PostCard/index.js
@@ -1,20 +1,18 @@
-/* eslint-disable jsx-a11y/alt-text */
-import { Link } from 'react-router-dom'
+import { Link as MuiLink } from '@mui/material'
 import styles from './post.module.css'
 
 export default function PostCard({ post }){
     return (
-        <Link to={`/posts/${post.id}`}>
+        <MuiLink href={`/posts/${post.id}`} underline="none">
             <div className={styles.post}>
                 <img className={styles.capa} 
                     src={`/assets/posts/${post.id}/capa.png`}
                     alt="Imagem de capa do post"
                 />
-
                 <h2 className={styles.titulo}>{post.titulo}</h2>
-
+                <h2 className={styles.descricao}>{post.texto}</h2>
                 <button className={styles.botaoLer}>Conhecer</button>
             </div>
-        </Link>
+        </MuiLink>
     )
 }
